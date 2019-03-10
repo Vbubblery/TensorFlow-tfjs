69c26225a7051a32e2180d5e4d2d0aba021cef4f
Vbubblery <zhoujuncheng0116@gmail.com>
2019-03-10 04:01:49 +0100

    finish

--- Iris/iris.js
+++ Iris/iris.js
@@ -28,19 +28,28 @@ const df_y = df.subset(["variety"]).select(row=>{
 }).getSeries("variety").toArray();
 
 const train_X = tf.tensor2d(df_X);
-const train_y = tf.tensor1d(df_y,dtype='int32').oneHot(train_y, 3);
-
-// train_y.print()
+const train_y = tf.oneHot(tf.tensor1d(df_y,dtype='int32'), 3);
 
 //
-// const input = tf.input({shape:[4]})
-// const layer1 = tf.layers.dense({units: 20, activation: 'relu'});
-// const layer2 = tf.layers.dense({units: 40, activation: 'relu'});
-// const layer3 = tf.layers.dense({units: 40, activation: 'relu'});
-// const layer4 = tf.layers.dense({units: 20, activation: 'relu'});
-// const layer5 = tf.layers.dense({units: 10, activation: 'relu'});
-// const layer6 = tf.layers.dense({units: 3, activation: 'softmax'});
-//
+const input = tf.input({shape:[3]})
+const layer1 = tf.layers.dense({units: 20, activation: 'relu'});
+const layer2 = tf.layers.dense({units: 40, activation: 'relu'});
+const layer3 = tf.layers.dense({units: 40, activation: 'relu'});
+const layer4 = tf.layers.dense({units: 20, activation: 'relu'});
+const layer5 = tf.layers.dense({units: 10, activation: 'relu'});
+const layer6 = tf.layers.dense({units: 3, activation: 'softmax'});
+
+const output = layer6.apply(layer5.apply(layer4.apply(layer3.apply(layer2.apply(layer1.apply(input))))));
+
+const model = tf.model({inputs: input, outputs: output});
+model.compile({optimizer: 'adam', loss: 'categoricalCrossentropy',metrics: ['accuracy']});
+
+model.fit(train_X, train_y, {
+  epochs: 100,
+  callbacks: {
+    onEpochEnd: (epoch, log) => console.log(`Epoch ${epoch}: loss = ${log.loss}`)
+  }
+});
 // // 调用dispose来清空已占用的 GPU 内存：
 // train_X.dispose();
 // train_y.dispose();
